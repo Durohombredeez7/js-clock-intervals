2f9ba8f8327896f635b3023c05ef984fad1fb071
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-11-19 08:53:43 -0800

    frozenclock

---
+++ clock.js
@@ -0,0 +1,15 @@
+// set up my variables
+var secondHand = document.getElementByIdName("second");
+var minuteHand = document.getElementByIdName("minute");
+var hourHand = document.getElementByIdName("hour")
+function moveHands = "
+
+// apply them to the rotate elements
+var degrees = 45;
+secondHand.style.transform = "rotate(" + degrees + "deg)";
+minuteHand.style.transform = "rotate(" + degrees + "deg)";
+hourHand.style.transform = "rotate(" + degrees + "deg)";
+// setup the first hint
+// var interval = setInterval(secondRotation() {
+//     console.log("TICK!");
+// }, 1000);
